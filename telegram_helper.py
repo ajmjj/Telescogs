7d552f4185d4cc8b07b18b35761841f0bc4888f4
Anton Jurgens <antonj@me.com>
2022-11-08 13:52:07 +0100

    Catch error if chat id entered for channel

--- telegram_helper.py
+++ telegram_helper.py
@@ -8,15 +8,32 @@ def get_chat_from_id(chat_id, chats):
 def select_chat(chats):
     chat_input = input('Type the name or the Id: ')
 
-    if chat_input[0] == '-' and chat_input[1:].isdigit():
+    # if chat_input[0] == '-' and chat_input[1:].isdigit():
+    if chat_input.isdigit():
         print(f'\nInput type: Chat Id')
         chat_input = int(chat_input) # make chat_input an integer
 
         if chat_input in [chat['id'] for chat in chats]:
             print(f'Entered chat Id: {chat_input}')
-            print(f'Chat found: {get_chat_from_id(chat_input, chats).get("name")}')
+            chat_name = get_chat_from_id(chat_input, chats).get("name")
+            print(f'Chat found: {chat_name}')
             print()
-            return chat_input
+            return chat_input, chat_name
+        elif chat_input*-1 in [chat['id'] for chat in chats]:
+            print("Your entered Id doesn't correspond to a chat, though it corresponds to a channel")
+            possible_chat_name = get_chat_from_id(chat_input*-1, chats).get("name")
+            print(f"Did you mean to enter the channel: {possible_chat_name}?")
+            res = input("Enter 'y' to select the channel, or 'n' to try again: ")
+            if res.strip().lower() == 'y' or res.strip().lower() == 'yes':
+                corrected_chat_id = chat_input *-1
+                chat_name = possible_chat_name
+                print(f'Corrected channel Id: {corrected_chat_id}')
+                print(f'Channel found: {chat_name}')
+                print()
+                return corrected_chat_id, chat_name
+            elif res.strip().lower() == 'n' or res.strip().lower() == 'no':
+                print("Ok, try again")
+                select_chat(chats)
         else: 
             print('\nInvalid chat Id, try again')
             select_chat(chats)
@@ -24,9 +41,10 @@ def select_chat(chats):
         print(f'\nInput type: Chat name')
         try:
             selected_chat_id = int([chat.get('id') for chat in chats if chat_input.lower().strip() == chat['name'].lower().strip()][0])
-            print(f'Chat found: {get_chat_from_id(selected_chat_id, chats).get("name")}')
+            chat_name = get_chat_from_id(selected_chat_id, chats).get("name")
+            print(f'Chat found: {chat_name}')
             print()
-            return selected_chat_id
+            return selected_chat_id, chat_name
         except IndexError: # index error because no chats in list -> if chat name exists, list should have one element only # todo -> add verification for one item in list
             print('\nInvalid chat name, try again')
             select_chat(chats)
